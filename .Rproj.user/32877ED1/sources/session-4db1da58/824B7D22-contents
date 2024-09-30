918abc716535336f736baa728c30f6b7ce5864e5
Maria <maria.cardelino@emory.edu>
2024-09-29 23:45:40 -0400

    Initial commit

---
+++ .Rproj.user/32877ED1/sources/session-4db1da58/824B7D22-contents
@@ -0,0 +1,190 @@
+#process all data from get_ceams_data
+#This script is scheduled to run every 3 days on my task scheduler
+rm(list = ls())
+library(dplyr)
+library(lubridate)
+source("get_ceams_data.R")
+
+# List of device IDs
+device_ids <- c(
+  # Pretoria, South Africa
+  'AD00103', 'AD00104', 'AD00405',
+  # Taichung, Taiwan
+  'AD00094',
+  # Tainan, Taiwan
+  'AD00092','AD00101','AD00403','AD00406',
+  # Delhi, India
+  'AD00096',
+  # Amity University Noida, India (88 starting in 2024)
+  'AD00088','AD00090','AD00413','AD00414',
+  # Amity University Manesar, India
+  'AD00408','AD00409',
+  # Adama, Ethiopia
+  'AD00410','AD00412',
+  #AASTU Addis Ababa, Ethiopia
+  'AD00091','AD00098','AD00093') 
+
+all_data <- get_ceams_data(device_ids) #call function in get_ceams_data.R
+
+# Keep only the specified columns
+all_data <- all_data %>%
+  select(cartridge_id, latitude, longitude, sn, timestamp_utc, pm25, fdpdp, volumetric_flowrate, pump_rh, pump_t)  
+
+all_data <- rename(all_data, 
+                 lat = latitude,
+                 lng = longitude,
+                 device = sn,
+                 time = timestamp_utc,
+                 filter = cartridge_id,
+                 flow = volumetric_flowrate,
+                 pressure = fdpdp,
+                 rh = pump_rh,
+                 t = pump_t,
+                 pm = pm25)
+  
+all_data <- all_data %>%
+    filter(!is.na(time)) %>%
+    filter(pm > 0) %>%
+    filter(!is.na(pm)) %>%
+    filter(pm < quantile(pm, 0.9999))
+
+all_data$time <- as.POSIXct(all_data$time, format= "%Y-%m-%dT%H:%M")
+  
+start_size = nrow(all_data) #size of all data pre-process
+
+#Group location by device ID ------------------
+  pretoria <- all_data %>%
+    filter(device == 'AD00103' | device == 'AD00104' | device == 'AD00405' )%>%
+    mutate(location = 'Pretoria, South Africa')
+  pretoria_recent <- pretoria %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Pretoria, South Africa')
+  
+  taichung <- all_data %>%
+    filter(device == 'AD00094')%>%
+    mutate(location = 'Taichung, Taiwan')
+  taichung_recent <- taichung %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Taichung, Taiwan')
+  
+  tainan <- all_data %>%
+    filter(device == 'AD00092' | device == 'AD00101' | device == 'AD00403' | device == 'AD00406')%>%
+    mutate(location = 'Tainan, Taiwan')
+  tainan_recent <- tainan %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Tainan, Taiwan')
+  
+  delhi <- all_data %>%
+    filter(device == 'AD00096')%>%
+    mutate(location = 'Delhi, India')
+  delhi_recent <- delhi %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Delhi, India')
+  
+  noida <- all_data %>%
+    filter(device == 'AD00088' | device == 'AD00090' | device == 'AD00413' | device == 'AD00414')%>%
+    mutate(location = 'Amity University Noida, India')
+  noida_recent <- noida %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Amity University Noida, India')
+  
+  manesar <- all_data %>%
+    filter(device == 'AD00408' | device == 'AD00409')%>%
+    mutate(location = 'Amity University Manesar, India')
+  manesar_recent <- manesar %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Amity University Manesar, India')
+  
+  adama <- all_data %>%
+    filter(device == 'AD00410' | device == 'AD00412')%>%
+    mutate(location = 'Adama, Ethiopia')
+  adama_recent <- adama %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'Adama, Ethiopia')
+  
+  aastu <- all_data %>%
+    filter(device == 'AD00091' | device == 'AD00098' | device == 'AD00093')%>%
+    mutate(location = 'AASTU, Ethiopia')
+  aastu_recent <- aastu %>%
+    filter(time >= Sys.Date() - 14)%>%
+    mutate(location = 'AASTU, Ethiopia')
+  
+all_recent_df <- rbind(pretoria_recent, taichung_recent, tainan_recent, delhi_recent, noida_recent, manesar_recent, adama_recent, aastu_recent)
+all_df <- rbind(pretoria, taichung, tainan, delhi, noida, manesar, adama, aastu) 
+
+#write all data tables
+write.csv(all_recent_df, "all_recent_data.csv", row.names = FALSE) #for plot
+write.csv(all_df, "all_data.csv", row.names = FALSE) #all data 
+
+#Run this if not pulling from get_ceams_data
+all_data <- read.csv("all_data.csv") #comment
+#summary statistics:
+
+#24-hour groups for recent data -----------------------------
+
+all_recent_df <- all_recent_df %>%
+  group_by(location, device, filter) %>% # group by location, device ID, filter ID
+  mutate(
+    start_time = min(time), #start time for cartridge ID
+    latest_time = max(time), #end time for cartridge ID
+    total_run_time = as.numeric(difftime(latest_time, start_time, units = "hours")), #total run time for cartridge ID
+    elapsed_time = as.numeric(difftime(time, start_time, units = "hours")) #elapsed time of current row for cartridge ID
+  ) %>%
+  mutate(interval = (elapsed_time %/% 24) + 1) %>% #create index for number of 24-hours periods (1-based)
+  ungroup()
+
+recent_daily <- all_recent_df %>% #create stats by interval
+  group_by(location, device, filter, interval) %>% #for each unique 24-hour interval by device...
+  
+  reframe(    start_time = min(time),
+              end_time = max(time),
+              n_readings = n(),
+              elapsed_time = difftime(end_time, start_time, units = c("hours")),
+              daily_mean_pm = mean(pm),
+              daily_95_pm = quantile(pm, .95),
+              daily_mean_rh = mean(rh),
+              daily_mean_t = mean(t))
+
+recent_daily <- recent_daily %>%
+  mutate(
+    day = as.Date(start_time),
+    across(c('elapsed_time', 'daily_mean_pm', 'daily_95_pm', 'daily_mean_rh', 'daily_mean_t'), round, 1)) %>%
+  select(location, device, filter, day, elapsed_time, n_readings, daily_mean_pm, daily_95_pm, daily_mean_rh, daily_mean_t)%>%
+  arrange(desc(day))
+
+#24-hour groups for ALL data -----------------------------
+all_df <- all_df %>%
+  group_by(location, device, filter) %>% # group by location, device ID, filter ID
+  mutate(
+    start_time = min(time), #start time for cartridge ID
+    latest_time = max(time), #end time for cartridge ID
+    total_run_time = as.numeric(difftime(latest_time, start_time, units = "hours")), #total run time for cartridge ID
+    elapsed_time = as.numeric(difftime(time, start_time, units = "hours")) #elapsed time of current row for cartridge ID
+  ) %>%
+  mutate(interval = (elapsed_time %/% 24) + 1) %>% #create index for number of 24-hours periods (1-based)
+  ungroup()
+
+all_daily <- all_df %>% #create stats by interval
+  group_by(location, device, filter, interval) %>% #for each unique 24-hour interval by device...
+  
+  reframe(    start_time = min(time),
+              end_time = max(time),
+              n_readings = n(),
+              elapsed_time = difftime(end_time, start_time, units = c("hours")),
+              daily_mean_pm = mean(pm),
+              daily_95_pm = quantile(pm, .95),
+              daily_mean_rh = mean(rh),
+              daily_mean_t = mean(t))
+
+all_daily <- all_daily %>%
+  mutate(
+    day = as.Date(start_time),
+    across(c('elapsed_time', 'daily_mean_pm', 'daily_95_pm', 'daily_mean_rh', 'daily_mean_t'), round, 1)) %>%
+  select(location, device, filter, day, elapsed_time, n_readings, daily_mean_pm, daily_95_pm, daily_mean_rh, daily_mean_t)%>%
+  arrange(desc(day))
+
+
+#write daily tables
+write.csv(recent_daily, "recent_daily.csv", row.names = FALSE) #for table
+write.csv(all_daily, "all_daily.csv", row.names = FALSE)
+   
---
+++ app.R
@@ -0,0 +1,531 @@
+#### BUILD APP ----------------------------------------------
+rm(list = ls())
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(shiny)
+library(bslib)
+library(ggplot2)
+library(readr)
+
+# Global variables for app
+locations <- c("Pretoria, South Africa", "Taichung, Taiwan", "Tainan, Taiwan", "Delhi, India", 
+               "Amity University Noida, India", "Amity University Manesar, India", "Adama, Ethiopia", 
+               "AASTU, Ethiopia")
+
+all_recent_df <- read_csv("all_recent_data.csv", show_col_types = FALSE) #for plotting recent data
+recent_daily <- read_csv("recent_daily.csv", show_col_types = FALSE) # for recent stats
+all_daily <- read_csv("all_daily.csv", show_col_types = FALSE)
+
+all_grav <- read_csv("all_grav.csv", show_col_types = FALSE) #for grav
+grav_summary <- read_csv("grav_summary.csv", show_col_types = FALSE) 
+
+# Define UI
+ui <- navbarPage(
+  title = 'AMOD/S Data Visualization App for MAIA + CEAMS',
+  
+  # Location selection at the top
+  tabPanel(
+    fluidPage(
+      fluidRow(
+        column(12,
+               selectInput("location", "Select A Location:", choices = locations, selected = "Amity University Noida, India")
+        )
+      )
+    )
+  ),
+  
+  # Panel 1: Recent Continuous data
+  tabPanel(
+    "Recent Plantower Data Visualization",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          h4("AMOD/S Continuous Data from Previous Two Weeks"),
+          radioButtons("plot_type", "Select Plot Type:",
+                       choices = list(
+                         "Timeseries for all variables" = "timeseries",
+                         "Dynamic plot with selectable variables" = "dynamic",
+                         "Co-location plot" = "colocation"
+                       ),
+                       selected = "timeseries")
+        ),
+        mainPanel(
+          uiOutput("paneloneUI")  # Dynamic UI for the combined panel
+        )
+      )
+    )
+  ),
+  
+  # Panel 2: Continuous Stats
+  tabPanel(
+    "24-hour Averaged Plantower Stats",
+    fluidPage(
+      # Add the date range selection controls in this panel
+      sidebarLayout(
+        sidebarPanel(
+          h4("Select timeframe for 24-hour averaged stats to display in table and figure"),
+          radioButtons("date_range", "Select Date Range:",
+                       choices = list("Past Two Weeks" = "two_weeks",
+                                      "Past Two Months" = "two_months",
+                                      "Custom" = "custom")),
+          uiOutput("custom_date_ui"),  # Conditionally display custom date range input
+          
+          p(strong("Note:"), "Averages shown for days with greater than 18 hours of continuous data.")
+        ),
+          
+        mainPanel(
+          plotOutput("pm25_plot"), #timeseries to match the table stats
+          uiOutput("tableUI"),  # Table to display filtered stats
+          
+        )
+      )
+    )
+  ),
+  
+  # Panel 3: Filter data
+  tabPanel(
+    "Gravimetric Filter Stats",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          h4("Gravimetric Filter Data Visualization"),
+          radioButtons("gravi_choice", "Select:",
+                       choices = list(
+                         "Summary table for filters (for selected location)" = "gravi_table",
+                         "Scatterplot of Continuous vs. Gravimetric concentrations (for selected location)" = "combined_plot",
+                         "All-location count and calendar for processed filters" = "calendar"
+                       ),
+                       selected = "gravi_table")
+        ),
+        mainPanel(
+          uiOutput("gravipanelUI")  # Dynamic UI for the combined panel
+        )
+      )
+    )
+  )
+) #end UI section
+
+# Define server logic
+server <- function(input, output, session) {
+  
+  # PANEL ONE: RECENT PLOTS ##############################
+    
+  # User selection combined UI #####
+  output$paneloneUI <- renderUI({
+    #timeseries
+    if (input$plot_type == "timeseries") {
+      tagList(
+        h4("Timeseries Plots for PM2.5 and Meteorological Data"),
+        br(),
+        
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("PM2.5 Concentration"),
+          plotOutput("pmPlot") ),
+        br(),
+        
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Flowrate"),
+          plotOutput("flowPlot") ),
+        br(), 
+        
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Pressure"),
+          plotOutput("pressurePlot") ),
+        br(),
+        
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Relative Humidity"),
+          plotOutput("rhPlot") ),
+        br(), 
+        
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Temperature"),
+          plotOutput("tPlot") ),
+        br()
+      )
+      
+    } else if (input$plot_type == "dynamic") {
+      tagList(
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Dynamic Timeseries Plot"),
+          uiOutput("dynamicUI"),  # Add this line to render the dynamic UI
+          plotOutput("dynamicPlot")
+        )
+      )
+    } else if (input$plot_type == "colocation") {
+      tagList(
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("PM2.5 Concentration between Co-located Devices (Previous Two Weeks)"),
+          plotOutput("coloplot") )
+      )
+    } 
+  }) #end panel one UI
+  
+  # Dynamic plot UI and rendering #####
+  
+  #Dynamic plot UI
+  output$dynamicUI <- renderUI({
+    # Dropdown for selection
+    fluidPage(
+      fluidRow(
+        column(12,
+               selectInput("variables", "Select Variables (up to 3):",
+                           choices = c("PM2.5 Concentration" = "pm", "Relative Humidity" = "rh", "Temperature" = "t"),
+                           selected = c("pm"),
+                           multiple = TRUE,
+                           selectize = TRUE)
+        )
+      )
+    )
+  }) #end dynamic plot UI
+  
+  # Render the dynamic plot
+  output$dynamicPlot <- renderPlot({
+    
+    req(input$variables)
+    
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    selected_vars <- input$variables
+    
+    # Check if selected variables are not empty
+    if (length(selected_vars) == 0) {
+      return(NULL)
+    }
+    
+    data_long <- selected_data %>%
+      select(time, device, all_of(selected_vars)) %>%
+      pivot_longer(cols = all_of(selected_vars), names_to = "Variable", values_to = "Value")
+    
+    color_mapping <- c(
+      "pm" = "red",
+      "rh" = "darkblue",
+      "t" = "black"
+    )
+    
+    legend_labels <- c(
+      "pm" = "PM2.5 Concentration",
+      "rh" = "Relative Humidity",
+      "t" = "Temperature"
+    )
+    
+    ggplot(data_long, aes(x = time, y = Value, color = Variable, shape = device)) +
+      geom_point() +
+      scale_color_manual(values = color_mapping, labels = legend_labels) +
+      labs(title = "Previous 2 Weeks Time Series Data", x = "Time", y = "Value") +
+      theme_grey() +
+      theme(legend.position = "bottom")
+  }) #End dynamic plot rendering
+  
+  
+  
+    # Timeseries plots for PM2.5 and meteorological data ##############
+  output$pmPlot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    ggplot(selected_data, aes(x = time, color = device)) +
+      geom_point(aes(y = pm)) +
+      labs(title = NULL, y = "PM2.5 (µg/m³)", x = "Time") +
+      theme_minimal()
+  })
+  
+  output$flowPlot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    ggplot(selected_data, aes(x = time, color = device)) +
+      geom_point(aes(y = flow)) +
+      labs(title = NULL, y = "Flowrate (L/min)", x = "Time") +
+      theme_minimal()
+  })
+  
+  output$pressurePlot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    ggplot(selected_data, aes(x = time, color = device)) +
+      geom_point(aes(y = pressure)) +
+      labs(title = NULL, y = "Pressure (dp)", x = "Time") +
+      theme_minimal()
+  })
+  
+  output$rhPlot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    ggplot(selected_data, aes(x = time, color = device)) +
+      geom_point(aes(y = rh)) +
+      labs(title = NULL, y = "Relative Humidity", x = "Time") +
+      theme_minimal()
+  })
+  
+  output$tPlot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    ggplot(selected_data, aes(x = time, color = device)) +
+      geom_point(aes(y = t)) +
+      labs(title = NULL, y = "Temperature (C)", x = "Time") +
+      theme_minimal()
+  })
+  
+    # Co-location plot #######################
+  output$coloplot <- renderPlot({
+    selected_data <- all_recent_df %>% filter(location == input$location)
+    
+    unique_devices <- unique(selected_data$device)
+    
+    if (length(unique_devices) >= 2) {
+      pm_data_device1 <- selected_data %>%
+        filter(device == unique_devices[1]) %>%
+        select(time, pm_device1 = pm)
+      
+      pm_data_device2 <- selected_data %>%
+        filter(device == unique_devices[2]) %>%
+        select(time, pm_device2 = pm)
+      
+      merged_pm_data <- inner_join(pm_data_device1, pm_data_device2, by = "time")
+      
+      ggplot(merged_pm_data, aes(x = pm_device1, y = pm_device2)) +
+        geom_point() +
+        labs(title = paste("Device", unique_devices[2], "vs.", unique_devices[1]),
+             x = paste("PM2.5 Concentration (µg/m³), Device", unique_devices[1]),
+             y = paste("PM2.5 Concentration (µg/m³), Device", unique_devices[2])) +
+        geom_abline(slope = 1, intercept = 0, color = "grey", linetype = "dashed") +
+        theme_grey()
+    } else {
+      ggplot() + geom_blank() +
+        labs(title = "Not enough devices for comparison")
+    }
+  })
+  
+  
+  
+  ## PANEL TWO: STATS #####################################
+  
+  # Date range UI for the second panel ####
+  output$custom_date_ui <- renderUI({
+    if (input$date_range == "custom") {
+      dateRangeInput("custom_dates", "Select Date Range:",
+                     start = Sys.Date() - 30, end = Sys.Date())
+    }
+  })
+  
+  # Filtering based on date selection
+  output$tableUI <- renderTable({
+    # Filter data by location
+    filtered_data <- all_daily %>% filter(location == input$location)
+    
+    # Date filtering based on selection
+    if (input$date_range == "two_weeks") {
+      filtered_data <- filtered_data %>%
+        filter(day >= Sys.Date() - 14 & day <= Sys.Date())
+    } else if (input$date_range == "two_months") {
+      filtered_data <- filtered_data %>%
+        filter(day >= Sys.Date() - 60 & day <= Sys.Date())
+    } else if (input$date_range == "custom") {
+      filtered_data <- filtered_data %>%
+        filter(day >= input$custom_dates[1] & day <= input$custom_dates[2])
+    }
+    
+    # Formatting data
+    formatted_data <- filtered_data %>%
+      mutate(
+        day = format(as.Date(day), "%Y-%m-%d"),
+        n_readings = format(round(n_readings, digits = 0), nsmall = 0),
+        elapsed_time = format(round(elapsed_time, digits = 0), nsmall = 0),
+        daily_mean_pm = format(round(daily_mean_pm, digits = 1), nsmall = 1),
+        daily_95_pm = format(round(daily_95_pm, digits = 1), nsmall = 1),
+        daily_mean_rh = format(round(daily_mean_rh, digits = 1), nsmall = 1),
+        daily_mean_t = format(round(daily_mean_t, digits = 1), nsmall = 1)
+      )
+    
+    colnames(formatted_data) <- c('Location', 'Device', 'Filter', 'Day', 'Elapsed Time (Hours)', 'Total # Readings', 'Daily Mean PM2.5 Conc. (µg/m³)', '...95th pctl', 'Daily Mean RH', 'Daily Mean Temp (C)')
+    
+    formatted_data # Show table
+  })
+  
+  # Timeseries plot for selected dates ####
+  output$pm25_plot <- renderPlot({
+    filtered_data <- all_daily %>% 
+      filter(location == input$location) %>%
+      filter(elapsed_time > 18 & n_readings > 30)
+    
+    #edit dataframe so days with much time in between are not connected by line
+    # TO- DO?
+    
+    if (input$date_range == "two_weeks") {
+      filtered_data <- filtered_data %>%
+        filter(day >= Sys.Date() - 14 & day <= Sys.Date())
+    } else if (input$date_range == "two_months") {
+      filtered_data <- filtered_data %>%
+        filter(day >= Sys.Date() - 60 & day <= Sys.Date())
+    } else if (input$date_range == "custom") {
+      filtered_data <- filtered_data %>%
+        filter(day >= input$custom_dates[1] & day <= input$custom_dates[2])
+    }
+    
+    #Plot
+    ggplot(filtered_data, aes(x = as.Date(day), y = daily_mean_pm, colour = device)) +
+      geom_line(linetype = "dashed") +
+      geom_point()+
+      labs(title = "24-h Averaged Continuous PM2.5 Concentration (µg/m³)", x = "Day", y = "PM2.5 (µg/m³)")+
+      theme_minimal()
+  })
+  
+  # PANEL THREE: GRAVIMETRIC #################
+  
+  #uiOutput: gravipanelUI (all choices), gravi_choice
+  #choices: "gravi_table", "combined_plot", "calendar"
+  
+  # User selection combined UI #####
+  output$gravipanelUI <- renderUI({
+    #timeseries
+    if (input$gravi_choice == "gravi_table") {
+      tagList(
+        h4("Gravimetric Filters Analyzed from Selected Location"),
+        uiOutput("gtable")  
+      )
+    } else if (input$gravi_choice == "combined_plot") {
+      tagList(
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Continuous Vs. Gravimetric PM2.5 Concentration at Selected Location"),
+          plotOutput("combinedgplot") 
+        )
+      )
+    } else if (input$gravi_choice == "calendar") {
+      tagList(
+        h4("All-location Summary Statistics"),
+        uiOutput("sumtable"),
+        br(),
+        wellPanel(
+          style = "border: 1px solid #000; padding: 10px;",
+          h4("Calendar of All Processed Gravimetric Filters By Device"),
+          plotOutput("gcalendar")) #,
+       #  br(),
+       #  wellPanel(
+       #    style = "border: 1px solid #000; padding: 10px;",
+       #    h4("Calendar of All Data By Method"),
+       #    plotOutput("allcalendar")),
+      )
+    } 
+  }) #end gravimetric panel UI
+  
+  # Option 1: table of data by selected location
+  output$gtable <- renderTable({
+    
+    # Filter all gravimetric data by location
+    filtered_grav <- all_grav %>% filter(Location == input$location)
+    filtered_grav <- filtered_grav %>% select(`Filter ID`, Device, Day, `PM2.5 Concentration (µg/m³)`, Location) %>%
+      mutate(
+        Day = format(as.Date(Day), "%Y-%m-%d"),
+        `PM2.5 Concentration (µg/m³)` = format(round(`PM2.5 Concentration (µg/m³)`, digits = 1), nsmall = 0))
+    filtered_grav
+
+  })
+  
+  # Option 2: Continuous vs. gravimetric
+  #Datasets: all_grav and all_daily
+  output$combinedgplot <- renderPlot({
+    
+    filtered_all_daily <- all_daily %>% 
+      filter(location == input$location)
+    
+    filtered_all_grav <- all_grav %>%
+      filter(Location == input$location) %>%
+      rename(grav = "PM2.5 Concentration (µg/m³)")
+  
+  matched_data <- filtered_all_grav %>%
+    inner_join(filtered_all_daily, by = c("Day" = "day", `Filter ID` = "filter")) 
+ 
+  p <- ggplot(matched_data, aes(x = grav, y = daily_mean_pm, color = device)) +
+    geom_point(size = 2.5) +
+    geom_vline(xintercept = 12, linetype = "dashed", color = "gray", size = 0.5) +
+    geom_hline(yintercept = 12, linetype = "dashed", color = "gray", size = 0.5) +
+    annotate("segment", x = 0, y = 0, xend = 200, yend = 200, color = "gray") +  # Fixed segment for diagonal line
+    labs(title = "Daily Averaged Continuous PM2.5 vs. Daily Gravimetric PM2.5 Concentrations",
+         x = "Daily Gravimetric Concentration (µg/m³)",
+         y = "Daily Averaged Continuous PM2.5 (µg/m³)",
+         color = "Device")+
+    scale_colour_brewer(palette = "Set1")
+  p #maybe change to include all if there are too little points for each location?
+  })
+  
+  #Option 3: Calendar and summary statistics
+  #summary table; all summary data
+  output$sumtable <- renderTable({
+    
+  grav_summary <- grav_summary %>%
+    mutate(
+      `Number of valid filters` = format(round(`Number of valid filters`, digits = 0), nsmall = 0),
+      `Mean PM2.5 Concentration (µg/m³)` = format(round(`Mean PM2.5 Concentration (µg/m³)`, digits = 1), nsmall = 0),
+      `Median` = format(round(`Median`, digits = 1), nsmall = 0),
+      `75th percentile` = format(round(`75th percentile`, digits = 1), nsmall = 0),
+      `95th percentile` = format(round(`95th percentile`, digits = 1), nsmall = 0),
+      `Max` = format(round(`Max`, digits = 1), nsmall = 0))
+  grav_summary 
+  })
+  
+  #calendar; all gravimetric data
+  # Add a new column to determine the color based on PM
+  output$gcalendar<- renderPlot({
+    
+    all_grav <- all_grav %>%
+      mutate(color = case_when(
+        `PM2.5 Concentration (µg/m³)` > 140 ~ "red",
+        `PM2.5 Concentration (µg/m³)` >= 40 & `PM2.5 Concentration (µg/m³)` <= 140 ~ "orange",
+        TRUE ~ "black"  # default color
+      ))
+  
+  # PLOT ###
+  c <- ggplot(all_grav, aes(x = Day, y = Device, color = color)) +
+    geom_point(size = 2) +
+    scale_x_date(date_labels = "%b %Y", date_breaks = "3 months", minor_breaks = "1 month") +
+    scale_color_manual(
+      name = "24-h mean PM2.5 concentration",
+      values = c("black" = "black", "orange" = "orange", "red" = "red"),
+      breaks = c("black", "orange", "red"),
+      labels = c("Less than 40 µg/m³", "40-140 µg/m³", "Greater than 140 µg/m³")) + 
+    labs(x = NULL, y = "Device", title = "Temporal Coverage of Device by Location") +
+    theme_minimal() + 
+    theme(legend.position = "top") 
+  c
+  })
+  
+  # output$allcalendar<- renderPlot({
+  #   #calendar; all grav and daily continuous data
+  # 
+  #   g <- all_grav %>%
+  #     rename(Gravimetric = "PM2.5 Concentration (µg/m³)", 
+  #            day = "Day", 
+  #            device = "Device", 
+  #            location = "Location") 
+  #   
+  #   d <- all_daily %>%
+  #     rename(Continuous = daily_mean_pm)
+  #   
+  #   all_data <- full_join(d, g, by = "day")
+  #   
+  #   all_data_long <- all_data %>%
+  #     pivot_longer(cols = c(Gravimetric, Continuous), names_to = "Method", values_to = "Value")
+  #   
+  #   # PLOT ###
+  #   ac <- ggplot(all_data_long, aes(x = day, y = device.x, color = Method)) +
+  #     geom_point(size = 2) +
+  #     scale_x_date(date_labels = "%b %Y", date_breaks = "3 months", minor_breaks = "1 month") +
+  #     labs(x = NULL, y = "Device", title = "Temporal Coverage of Device by Location") +
+  #     scale_color_manual(
+  #       name = "Method",
+  #       values = c("Gravimetric" = "darkblue", "Continuous" = "green"),  # Correctly mapping method names to colors
+  #       breaks = c("Gravimetric", "Continuous"),  # Ensuring the legend uses method names
+  #       labels = c("Gravimetric", "Continuous")) + 
+  #     theme_minimal() + 
+  #     theme(legend.position = "top") 
+  #   
+  #   ac
+  # })
+  
+}
+# END SERVER LOGIC #######
+
+# Run the application 
+shinyApp(ui = ui, server = server)
