d3406fa05a008ddbef9ac9496baf1400dfe95992
mariacardelino <maria.cardelino@emory.edu>
2025-05-08 11:32:08 -0400

    Update run_local.R

--- run_local.R
+++ run_local.R
@@ -1,4 +1,4 @@
 ## If you want to run the app from R Studio on your local machine, you can use the following code:
 
 library(shiny)
-runGitHub(repo = "maia_shiny", username = "mariacardelino", ref = "main")
+runGitHub(repo = "maia_dashboard", username = "mariacardelino", ref = "main")
